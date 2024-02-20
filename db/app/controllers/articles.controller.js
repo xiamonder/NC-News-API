21ba5af2f6e691c8bfa9d81bb5fbaf324e2f049c
xiamonder <acldiamond@gmail.com>
2024-02-20 13:37:02 +0100

    completed task 5, get articles sorted by date

--- db/app/controllers/articles.controller.js
+++ db/app/controllers/articles.controller.js
@@ -1,10 +1,20 @@
-const { fetchArticleById } = require("../models/articles.model");
+const { fetchArticleById, fetchArticles } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticles = (req, res, next) => {
+  fetchArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
     })
     .catch((err) => {
       next(err);
