78bcba08204103354da29148ef6cedef22977541
xiamonder <acldiamond@gmail.com>
2024-02-21 16:00:07 +0100

    hosting debugging, created listen.js

--- db/app/app.js
+++ db/app/app.js
@@ -21,10 +21,6 @@ const { getUsers } = require("./controllers/users.controller.js");
 
 const app = express();
 
-const { PORT = 9090 } = process.env;
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-
 app.use(express.json());
 
 app.get("/api", getAPI);
