7ced4f177ddbc6920b8ca61937179709c37868e3
xiamonder <acldiamond@gmail.com>
2024-02-22 15:01:47 +0100

    added get user by username functionality

--- db/app/models/users.model.js
+++ db/app/models/users.model.js
@@ -5,3 +5,14 @@ exports.fetchUsers = () => {
     return rows;
   });
 };
+
+exports.fetchUserByUsername = (username) => {
+  return db
+    .query(`SELECT * FROM users WHERE username = $1`, [username])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "user not found" });
+      }
+      return rows[0];
+    });
+};
--- db/app/routers/users.router.js
+++ db/app/routers/users.router.js
@@ -1,8 +1,10 @@
-const { getUsers } = require("../controllers/users.controller");
+const { getUsers, getUserByUsername } = require("../controllers/users.controller");
 
 const userRouter = require("express").Router();
 
 userRouter.get("/", getUsers);
 
-
+userRouter
+.route('/:username')
+.get(getUserByUsername)
 module.exports = userRouter;
