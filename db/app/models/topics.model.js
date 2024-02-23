b30644ce78c9b6740ee1fe6ea6c15dda2dd3e453
xiamonder <acldiamond@gmail.com>
2024-02-23 16:17:49 +0100

    added post topic functionality

--- db/app/models/topics.model.js
+++ db/app/models/topics.model.js
@@ -1,4 +1,5 @@
 const db = require("../../connection");
+const format = require("pg-format");
 
 exports.fetchTopics = () => {
   return db.query(`SELECT * FROM topics`).then(({ rows }) => {
@@ -16,3 +17,23 @@ exports.fetchTopicBySlug = (topic) => {
       return rows[0];
     });
 };
+
+exports.addTopic = (slug, description) => {
+
+    const queryString = format(
+      `INSERT INTO topics (slug, description) VALUES %L RETURNING *;`,
+      [[slug, description]]
+    );
+    console.log(queryString)
+    return db
+      .query(queryString)
+      .then(({ rows }) => {
+        return rows[0];
+      })
+      .catch((err) => {
+        console.log(err)
+        if (err.code === "23505") {
+          return Promise.reject({ status: 400, msg: "topic already exists" });
+        } else return Promise.reject(err);
+      });
+  };
